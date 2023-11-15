95f435b3fd79f364da78d040dbcbf44a41123b4e
asingh824 <66382147+asingh824@users.noreply.github.com>
2023-11-15 12:06:55 -0800

    Update gitCheckout.groovy

--- gitCheckout.groovy
+++ gitCheckout.groovy
@@ -1,8 +1,1 @@
-def call(Map stageParams) {
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+
